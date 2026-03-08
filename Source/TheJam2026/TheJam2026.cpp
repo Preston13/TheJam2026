2c10cd24401fb3fb30d331df0d3ba0a991fe6851
Preston Grau <preston.grau@gmail.com>
2026-03-08 11:12:44 -0500

    initial commit

---
+++ Source/TheJam2026/TheJam2026.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TheJam2026.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TheJam2026, "TheJam2026" );
